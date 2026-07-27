500cd0bc579b1d664986dd71f5c4b1b1405711b5
PostCargo <asistenciagerenciageneral2013@gmail.com>
2026-07-27 08:32:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ st.markdown(
 
     /* ---- Logo corporativo súper pequeño ---- */
     .pc-logo-wrap img {
-        max-width: 90px !important;
+        max-width: 170px !important;
         height: auto !important;
         display: block !important;
     }
