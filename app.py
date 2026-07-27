922cea71391f48c936e4364c96cb0fcb16fdce1d
PostCargo <asistenciagerenciageneral2013@gmail.com>
2026-07-27 13:50:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -259,8 +259,6 @@ st.markdown(
     f"""
     <p class="pc-contact-item">📧&nbsp;&nbsp;E-Mail: <a href="mailto:{EMAIL}">{EMAIL}</a></p>
     <p class="pc-contact-item">📱&nbsp;&nbsp;Cel: <a href="tel:{CEL_LINK}">{CEL_TEXTO}</a></p>
-    <p class="pc-contact-item">☎️&nbsp;&nbsp;Tel: <a href="tel:{TEL_LINK}">{TEL_TEXTO}</a></p>
-    <p class="pc-contact-item">🏙️&nbsp;&nbsp;Bogotá - Colombia</p>
     <p class="pc-contact-item">🌐&nbsp;&nbsp;Web: <a href="{WEB_URL}" target="_blank">www.postcargo.co</a></p>
     <p class="pc-contact-item">📍&nbsp;&nbsp;Dirección: <a href="{MAPS_URL}" target="_blank">{DIRECCION}</a></p>
     """,
